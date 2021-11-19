b9e7b787141eebf4ebddb4c904a523674d4becb9
hibeep <jctpe8888@gmail.com>
2021-11-19 13:57:12 +0000

    Task 6

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 //The variables from before should now be taken in as parameters 
 //so that you can call the function with any two moves.
 //Instead of printing the result to the console, the function should return:
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
+
+
+
+
+
+
 function getWinner(playerMove,computerMove) {
 
 //let playerMove = "paper";
@@ -9,14 +19,17 @@ function getWinner(playerMove,computerMove) {
 
 if (playerMove  ===  computerMove){
 //console.log("it's a draw");
+draws ++;
 return 0;
 } else if (playerMove  ===  "rock"){
     if (computerMove === "paper"){
     //console.log("computerMove win");
-    return -1;
+    losses ++;
+    return -1; 
     }
     else if (computerMove === "scissors"){
     //console.log("playerMove win");
+    wins ++;
     return 1;
     }
     
@@ -24,10 +37,12 @@ return 0;
 else if (playerMove  ===  "paper"){
     if (computerMove === "scissors"){
    // console.log("computerMove win");
+   losses ++;
    return -1;
     }
     else if (computerMove === "rock"){
     //console.log("playerMove win");
+    wins ++;
     return 1;
     }
     
@@ -35,20 +50,30 @@ else if (playerMove  ===  "paper"){
 else if (playerMove  ===  "scissors"){
     if (computerMove === "rock"){
     //console.log("computerMove win");
+    losses ++;
     return -1;
     }
     else if (computerMove === "paper"){
     //console.log("playerMove win");
+    wins ++;
     return 1;
     }
     
 }
 }
-
+let confirm = "yes";
+let counts = 0;
+while (confirm === "yes") {
 let computerInput = ["rock","paper","scissors"];
 let randomNumber = Math.floor((Math.random() * 3));
 
 let playerInput = prompt("Enter your move.");
 let result = getWinner(playerInput, computerInput[randomNumber]);
-alert(result);
-
+counts ++;
+alert(`Result: ${result}, Counts: ${counts}, Wins: ${wins},
+ Losses${losses}, Draws: ${draws}`);
+confirm = prompt ("Would you like to play again? ");
+}
+// track how many games played
+// track numbers of wins, losses, draws 
+// display after each round
