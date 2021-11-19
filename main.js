88f07dddb99703ce7567dfe65d9362245bfb5805
lkepitter <lkepitter@hotmail.co.uk>
2021-11-19 11:51:12 +0000

    completed up to Task 4

--- main.js
+++ main.js
@@ -1,36 +1,54 @@
-//to write a set of if statements that will determine 
-//the winner of rock, paper, scissors
-let playerMove = "paper";
-let computerMove = "paper";
-// rock paper paper win
-// rock scissor rock win
-// scissor paper scissor win
+
+//The variables from before should now be taken in as parameters 
+//so that you can call the function with any two moves.
+//Instead of printing the result to the console, the function should return:
+function getWinner(playerMove,computerMove) {
+
+//let playerMove = "paper";
+//let computerMove = "paper";
+
 if (playerMove  ===  computerMove){
-console.log("it's a draw");
+//console.log("it's a draw");
+return 0;
 } else if (playerMove  ===  "rock"){
     if (computerMove === "paper"){
-    console.log("computerMove win");
+    //console.log("computerMove win");
+    return -1;
     }
     else if (computerMove === "scissors"){
-    console.log("playerMove win");
+    //console.log("playerMove win");
+    return 1;
     }
     
 }
 else if (playerMove  ===  "paper"){
     if (computerMove === "scissors"){
-    console.log("computerMove win");
+   // console.log("computerMove win");
+   return -1;
     }
     else if (computerMove === "rock"){
-    console.log("playerMove win");
+    //console.log("playerMove win");
+    return 1;
     }
     
 }
 else if (playerMove  ===  "scissors"){
     if (computerMove === "rock"){
-    console.log("computerMove win");
+    //console.log("computerMove win");
+    return -1;
     }
     else if (computerMove === "paper"){
-    console.log("playerMove win");
+    //console.log("playerMove win");
+    return 1;
     }
     
 }
+}
+
+let computerInput = ["rock","paper","scissors"];
+let randomNumber = Math.floor((Math.random() * 3));
+
+let playerInput = prompt("Enter your move.");
+let result = getWinner(playerInput, computerInput[randomNumber]);
+alert(result);
+
